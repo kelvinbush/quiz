32879e983b58bdbfb7e2066d4fe2dfc37fc7a6e4
Kelvin Bush <kelybush@gmail.com>
2021-07-09 15:15:28 +0300

    adding icons
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List scoreKeeper = [];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -84,7 +86,12 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: [
+            Icon(Icons.check, color: Colors.green),
+            Icon(Icons.close, color: Colors.red),
+          ],
+        )
       ],
     );
   }
