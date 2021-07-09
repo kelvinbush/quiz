94d83f268ba447d40b776b4b914a1ebb504cb0bc
Kelvin Bush <kelybush@gmail.com>
2021-07-09 15:58:19 +0300

    using classes
    Signed-off-by: Kelvin Bush <kelybush@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/Question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,12 +27,11 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    "A slug's blood is green."
+  List<Question> questionBank = [
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question("A slug's blood is green.", true)
   ];
-  List<bool> answer = [false, true, true];
 
   int questionNumber = 0;
 
@@ -47,7 +47,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,7 +71,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () => setState(() {
-                if (answer[questionNumber] == true) {}
+                if (questionBank[questionNumber].questionAnswer == true) {}
                 questionNumber++;
               }),
             ),
@@ -90,7 +90,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () => setState(() {
-                if (answer[questionNumber] == false) {}
+                if (questionBank[questionNumber].questionAnswer == false) {}
                 questionNumber++;
               }),
             ),
